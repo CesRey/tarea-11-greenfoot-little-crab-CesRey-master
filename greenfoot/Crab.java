c4d201d6b0ac8c53079d72da06055b83bbe933dd
ANDRES <and_araiza@hotmail.com>
2017-10-04 10:49:45 -0500

    Funciones movimiento
    Se agrego movimiento a Crab y Lobster

--- greenfoot/Crab.java
+++ greenfoot/Crab.java
@@ -8,19 +8,25 @@ public class Crab extends Actor
     @Override
     public void act()
     {
-        if (super.isTouching(null)){
-            //System.out.println("tocando algo");
-            super.removeTouching(null);
+       if (super.isTouching(Worm.class)){
+           //System.out.println("tocando algo");
+            super.removeTouching(Worm.class);
+       }
+       if(super.isAtEdge()){
+           super.turn(15);
+       }
+       //int aleatorio= Greenfoot.getRandomNumber(100);
+       /*if(aleatorio<10){
+           int grados=Greenfoot.getRandomNumber(90);
+           super.turn(grados-45);
+       }*/
+       if(Greenfoot.isKeyDown("right")){
+            super.turn(10);
         }
-        if(super.isAtEdge()){
-            super.turn(15);
+        else if (Greenfoot.isKeyDown("left")){
+            super.turn(-10);
         }
-        int aleatorio= Greenfoot.getRandomNumber(100);
-        if(aleatorio<10){
-            int grados=Greenfoot.getRandomNumber(90);
-            super.turn(grados-45);
-        }
-        super.move(10);
+       super.move(10);
     }
 }
 
---
+++ greenfoot/Lobster.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Lobster here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lobster extends Actor
+{
+    /**
+     * Act - do whatever the Lobster wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+       if(super.isAtEdge()){
+            super.turn(15);
+       }
+       if (super.isTouching(Crab.class)){
+           //System.out.println("tocando algo");
+            super.removeTouching(Crab.class);
+       }
+        int aleatorio= Greenfoot.getRandomNumber(100);
+        if(aleatorio<10){
+            int grados=Greenfoot.getRandomNumber(90);
+            super.turn(grados-45);
+        }
+        super.move(15);
+    }    
+}
